c7106e54a3cd06b846624e7464e98ccd6201df6a
malek-chebil <chebil1203@gmail.com>
2020-07-13 23:16:55 +0200

    ProjectC

--- js.js
+++ js.js
@@ -1,17 +1,18 @@
-
+/* onload to run the function after loading the page */
 window.onload = function () {
+/*to add new class "completed" in "ul" to style it with the bar*/
 
 $("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
-
+/* delate il when x is clicked*/
 $("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove()
     });
     event.stopPropagation();
 });
-
+/*add li to the list by pressing enter keyword ===13 */
 $("input[type='text'").keypress(function (event){
     if(event.which === 13){
         var todoText = $(this).val();
