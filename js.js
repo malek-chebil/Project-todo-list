f5816590905e33f1f6c6758fa789c52796f70732
malek-chebil <chebil1203@gmail.com>
2020-07-13 11:56:30 +0200

    Proj

---
+++ js.js
@@ -0,0 +1,22 @@
+
+window.onload = function () {
+
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove()
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function (event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        $(this).val("");
+        $("ul").append("<li> " + todoText +  "<span><em class = 'remove'>X</em></span></li>")
+    }
+});
+}
